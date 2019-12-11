c93171516a9f44bd97eec0d646d6e1f2c076ecd9
Carlos Ramirez <ramirezcoronelcarlos@gmail.com>
2019-12-11 16:51:51 -0400

    html-fixes

--- views/main/index.php
+++ views/main/index.php
@@ -12,28 +12,9 @@
   <?php require 'views/header.php'; ?> <!-- MENU -->
   <div class="container">
     <?php require 'views/menu.php'; ?> <!-- MENU -->
-
       <main>
-
         <div class="produccion">
-        <h2>Cantidad de Canaimas Producidas</h2>
-
-        <div class="produccion__equipos">
-
-          <div class="equipos__laptops">
-            <img src="<?php echo constant('URL')?>public/img/laptop.png" alt="Modelos de Laptops" />
-            <ul>
-              <li>Letras azules: 2034</li>
-              <li>Letras rojas: 123</li>
-              <li>Profesores: 334</li>
-            </ul>
-          </div>
-
-          <div class="equipos__tablets">
-            <img src="<?php echo constant('URL')?>public/img/tablet.png" alt="Modelos de tablet" />
-            <p>Tablets: 1340</p>
-          </div>
-        </div>
+        <h2>Seguimiento de Ensamblado y Control de Producción de Canaima</h2>
       </div>
       </main>
   </div>
