c00e2aae134459b0f5f890d10ad676a2dc8e7eb1
Carlos Ramirez <ramirezcoronelcarlos@gmail.com>
2019-12-11 14:51:22 -0400

    inventario-ajax-finished

--- views/inventario/consultarMovimientos.php
+++ views/inventario/consultarMovimientos.php
@@ -7,8 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>SECPC | Movimietos</title>
     <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/main.css">
-    <script src="<?php echo constant('URL')?>public/js/jquery-3.4.1.min.js"></script>
-
 </head>
 <body>
   <!-- Uso esta clase por el fondo rojo -->
@@ -17,33 +15,28 @@
     <?php require 'views/menu.php'; ?> <!-- MENU -->
     <main>
     <div class="text-header"><h2>Gestionar Movimientos</h2></div>
-    <div  id="form" data-eliminar="eliminarMovimiento" class="form">
+    <form  action="gestionar_usuarios.php" method="POST" class="form">
       <div class="form__box">
         <div class="centrar">
           <table class="tabla tabla-secundaria">
             <caption class="agrandar">Movimientos</caption>
-
-
             <tr> <th>Numero</th> <th>Tipo</th> <th>Fecha</th> <th>Hora</th> <th>Modificar</th> <th>Eliminar</th>
-            <tbody id="tbody-inventario">
-              <?php
+            <?php
 
-                foreach($this->movimientos as $row){
-                  $movimiento = new MovimientosClass();
-                  $movimiento = $row;
+              foreach($this->movimientos as $row){
+                $movimiento = new MovimientosClass();
+                $movimiento = $row;
 
-              ?>
-              </tr>
-              <tr id="fila-<?php echo $movimiento->getNumero()?>">
-                <td><?php echo $movimiento->getNumero(); ?></td>
-                <td><?php echo $movimiento->getTipo(); ?></td>
-                <td><?php echo $movimiento->getFecha(); ?></td>
-                <td><?php echo $movimiento->getHora(); ?></td>
-                <td><a class="botonForm" href="<?php echo constant('URL')?>inventario/actualizarMovimiento/<?php echo $movimiento->getNumero() ?>">Modificar</a></td>
-                <td><button class="botonForm eliminar" data-id="<?php echo $movimiento->getNumero(); ?>">Eliminar</button></td>
-              </tr>
-              <?php } ?>
-            </tbody>
+            ?>
+            </tr>
+              <td><?php echo $movimiento->getNumero(); ?></td>
+              <td><?php echo $movimiento->getTipo(); ?></td>
+              <td><?php echo $movimiento->getFecha(); ?></td>
+              <td><?php echo $movimiento->getHora(); ?></td>
+              <td><a class="botonForm" href="<?php echo constant('URL')?>inventario/actualizarMovimiento/<?php echo $movimiento->getNumero() ?>">Modificar</a></td>
+              <td><a class="botonForm" href="<?php echo constant('URL')?>inventario/eliminarMovimiento/<?php echo $movimiento->getNumero() ?>">Eliminar</a></td>
+            </tr>
+            <?php } ?>
           </table>
         </div>
       </div>
@@ -53,10 +46,9 @@
         <a href="<?php echo constant('URL')?>inventario" class="boton margin-lados">Volver</a>
       </div>
 
-    </div>
+      </form>
     </main>
   </div>
-  <script src="<?php echo constant('URL')?>public/js/AJAX/eliminar.js"></script>
 
 </body>
 </html>
