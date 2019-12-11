167b23522a0baaf5e70f4495b45d5ff7aa3a9b7b
Carlos Ramirez <ramirezcoronelcarlos@gmail.com>
2019-12-11 16:37:09 -0400

    html-fix

--- views/inventario/index.php
+++ views/inventario/index.php
@@ -5,7 +5,7 @@
     <link rel="shortcut icon" href="<?php echo constant('URL')?>public/img/logo3.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SECPC | Usuarios</title>
+    <title>SECPC | inventario</title>
     <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/main.css">
 </head>
 <body>
@@ -42,7 +42,7 @@
            <a href="<?php echo constant('URL')?>inventario/consultarMovimientos"><h2>Gestionar Movimientos</h2></a>
           </div>
         </div>
-        
+
       </div>
     </main>
   </div>
