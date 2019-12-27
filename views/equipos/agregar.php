fef1576b39d4799ec8bececfd8acba617fb6bd79
ramirez <ramirezcoronelcarlos@gmail.com>
2019-12-27 15:23:29 -0400

    redesign equipos

--- views/equipos/agregar.php
+++ views/equipos/agregar.php
@@ -5,7 +5,7 @@
     <link rel="shortcut icon" href="<?php echo constant('URL')?>public/img/logo3.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SECPC | Movimiento</title>
+    <title>SECPC | Equipos</title>
     <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/main.css">
 </head>
 <body>
@@ -21,16 +21,19 @@
 
             <form name="formulario" method="post" action="<?php echo constant('URL')?>equipos/agregar"  class="form" id="form">
 
-          <div class="form__box centrar">
-            <div class="margin-lados">
+           <div class="form-header"> <p>Agregar Equipo</p> </div>
+          
+
+          <div class="form__box">
+            <div>
               <label for="codtipoequipo ">Tipo Equipo</label>
               <input type="text" name="codtipoequipo" id="codtipoequipo" value="<?php echo $this->equipo->getCodTipo()?>"readonly>
             </div>
-            <div class="margin-lados">
+            <div>
               <label for="codequipo">codigo de Equipo:</label>
               <input type="text" name="codequipo" id="codequipo" value="<?php echo $this->equipo->getCodigo()?>" readonly/>
             </div>
-            <div class="margin-lados">
+            <div>
               <label for="nomequipo">Nombre de Equipo:</label>
               <input type="text" name="nomequipo" id="nomequipo" value="<?php echo $this->equipo->getNombre()?>"readonly/>
             </div>
@@ -41,8 +44,11 @@
 
         <form name="formulario" method="post" action="<?php echo constant('URL')?>equipos/agregar"  class="form" id="form">
 
-          <div class="form__box centrar">
-            <div class="margin-lados">
+           <div class="form-header"> <p>Agregar Parte</p> </div>
+          
+
+          <div class="form__box">
+            <div>
               <label for="codtipoequipo ">Tipo Equipo</label>
               <select name="codtipoequipo" class="select" id="select">
                 <option value="0">Seleccione</option>
@@ -55,19 +61,19 @@
             <?php } ?>
               </select>
             </div>
-            <div class="margin-lados">
+            <div>
               <label for="codequipo">codigo de Equipo:</label>
               <input type="text" name="codequipo" id="codequipo" />
             </div>
-            <div class="margin-lados">
+            <div>
               <label for="nomequipo">Nombre de Equipo:</label>
               <input type="text" name="nomequipo" id="nomequipo" />
             </div>
           </div>
 
-          <div class="esquinas">
-          <a href="<?php echo constant('URL')?>equipos/consultarEquipos" class="boton">Volver</a>
-          <button type="submit" name="agregar" class="boton">Agregar Tipo de Equipo</button>
+          <div class="bottom">
+          <a href="<?php echo constant('URL')?>equipos/consultarEquipos" >Volver</a>
+          <button type="submit" name="agregar" >Agregar Tipo de Equipo</button>
 
         </div>
         
@@ -81,9 +87,9 @@
       <?php if ($this->visible) {?>
 
         <h2 class="text-header margin-bottom">Agregar Parte A Equipo:</h2>
-         <div class="form__box centrar">
+         <div class="form__box">
          
-           <div class="margin-lados">
+           <div>
               <label for="codpartes ">Partes: </label>
               <select name="codpartes" class="select" id="select">
                 <option value="0">Seleccione</option>
@@ -96,15 +102,15 @@
             <?php } ?>
               </select>
            </div>
-          <div class="margin-lados">
+          <div>
             <label for="cantidadparteequipo">Cantidad:</label>
             <input type="text" name="cantidadparteequipo" id="cantidadparteequipo" />
           </div>
         </div>
 
-        <div class="centrar">
-          <a href="<?php echo constant('URL')?>equipos" class="boton">Listo</a>
-          <button type="submit" name="agregarParte" class="boton">Agregar Parte a Equipo</button>
+        <div class="bottom">
+          <a href="<?php echo constant('URL')?>equipos" >Listo</a>
+          <button type="submit" name="agregarParte" >Agregar Parte a Equipo</button>
         </div>
         
         
