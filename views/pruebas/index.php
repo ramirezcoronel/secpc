c1027d326246172a63bdeec09fd37fe03cf6f49b
ramirez <ramirezcoronelcarlos@gmail.com>
2020-01-07 17:28:13 -0400

    title issue

--- views/pruebas/index.php
+++ views/pruebas/index.php
@@ -5,7 +5,7 @@
     <link rel="shortcut icon" href="<?php echo constant('URL')?>public/img/logo3.png" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SECPC | Stressing</title>
+    <title>SECPC | Pruebas</title>
     <link rel="stylesheet" href="<?php echo constant('URL')?>public/css/main.css">
 </head>
 <body>
